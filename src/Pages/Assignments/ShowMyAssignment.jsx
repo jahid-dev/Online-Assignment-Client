e239889bf00e08b9de6c11efd01346c03805ef23
zinolot <mrjahidhassan0@gmail.com>
2023-11-07 03:33:41 +0600

    assignment all data dynamically working and my assignment working properly

--- src/Pages/Assignments/ShowMyAssignment.jsx
+++ src/Pages/Assignments/ShowMyAssignment.jsx
@@ -1,12 +1,25 @@
+const ShowMyAssignment = ({ showAssignments }) => {
+  const { status, name,marks,  feedback, givenMark, note } = showAssignments;
 
-
-const ShowMyAssignment = ({showMyTask}) => {
-    const {status} = showMyTask
-    return (
-        <div>
-            <h2>hello show may assignments</h2>
+  //title, feedback, obtain marsk, total marks, assignment status
+  return (
+    <div>
+      <div className="h-60 card-compact w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Assignment Title: {name} </h2>
+          <p className="text-xl font-semibold ">Total Marks: {marks}</p>
+          <p className="text-xl font-semibold ">
+          Obtain Marks: {status === "pending" ? "Pending" : givenMark}
+          
+           </p>
+          <p className="text-xl font-semibold ">Status: {status}</p>
+          <p className="text-xl ">Teacher Feedback: {status === "pending" ? "Pending" : feedback}</p>
+          
+          
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ShowMyAssignment;
--- src/Pages/MyAssignment/MyAssignment.jsx
+++ src/Pages/MyAssignment/MyAssignment.jsx
@@ -1,35 +1,34 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../firebase/Provider/AuthProvider";
-import ShowAssignments from "../Assignments/showAssignments";
+import ShowMyAssignment from "../Assignments/ShowMyAssignment";
 
 const MyAssignment = () => {
   const [showMyAssignment, setShowMyAssignment] = useState([]);
   const { user } = useContext(AuthContext);
 
-  // http://localhost:5000/api/v1/takenewassignments
   useEffect(() => {
     fetch("http://localhost:5000/api/v1/takenewassignments")
       .then((res) => res.json())
       .then((data) => {
-        const filterdMyAssignment = data.filter(
-          (MyAssignment) =>
-            MyAssignment?.submitUSerEmail == user?.email &&
-            MyAssignment?.status == "complete"
+        const filteredMyAssignment = data.filter(
+          (assignment) => assignment?.submitUserEmail === user?.email
         );
-        setShowMyAssignment(filterdMyAssignment);
+        setShowMyAssignment(filteredMyAssignment);
       });
-  });
+  }, []); // Empty dependency array
+
   return (
     <div>
-      <h1>This is My Assignment</h1>
-      {showMyAssignment.length}
+      <h2 className="text-5xl mb-10 mt-20 text-blue-500 font-bold text-center">
+        My All Assignments
+      </h2>
 
-      <div>
-        {showMyAssignment?.map((showAssignments) => (
-          <ShowAssignments
-            showMyTask={showAssignments}
+      <div className="max-w-[1200px] mx-auto grid gap-5 grid-cols-3">
+        {showMyAssignment.map((showAssignments) => (
+          <ShowMyAssignment
             key={showAssignments.id}
-          ></ShowAssignments>
+            showAssignments={showAssignments}
+          />
         ))}
       </div>
     </div>
