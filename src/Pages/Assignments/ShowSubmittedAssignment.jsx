ecca2a7d12abd8c33e6f390d9a5977551826b16e
zinolot <mrjahidhassan0@gmail.com>
2023-11-06 23:58:07 +0600

    submitted assignment showing

--- src/Pages/Assignments/ShowSubmittedAssignment.jsx
+++ src/Pages/Assignments/ShowSubmittedAssignment.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const ShowSubmittedAssignment = ({submit}) => {
-    const {} = submit
-    return (
-        <div>
-            
-        </div>
-    );
+const ShowSubmittedAssignment = ({ submit }) => {
+  const { name, assignmentId, date, status, marks, userEmail, note, pdfLink } = submit;
+  
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <tbody>
+            <tr className="bg-base-200 text-center">
+              <td>{name}</td>
+              <td>{marks}</td>
+              <td>{date}</td>
+              <td>{status}</td>
+              <td><button className="btn btn-primary">Give Mark</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ShowSubmittedAssignment;
--- src/Pages/SubmittedAssignments/SubmittedAssignments.jsx
+++ src/Pages/SubmittedAssignments/SubmittedAssignments.jsx
@@ -1,31 +1,43 @@
 import { useEffect, useState } from "react";
 import ShowSubmittedAssignment from "../Assignments/ShowSubmittedAssignment";
 
-
 const SubmittedAssignments = () => {
+  const [submitAssignment, setSubmitAssignment] = useState([]);
 
-    const [submitAssignment, setSubmitAssignment] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/takenewassignments")
+      .then((res) => res.json())
+      .then((data) => {
+        setSubmitAssignment(data);
+      });
+  }, []);
 
-    useEffect(() => {
+  return (
+    <div className="max-w-[1200px]  mt-20 mx-auto">
+      <table className="table ">
+        <thead>
+          <tr className="text-center">
+            
+            <th>Assignment Title</th>
+            <th>Mark</th>
+            <th>Due date</th>
+            <th>Status</th>
+            <th>Give Mark</th>
+          </tr>
+        </thead>
+        
+      </table>
 
-        fetch('http://localhost:5000/api/v1/takenewassignments')
-        .then(res => res.json())
-        .then(data => {
-            setSubmitAssignment(data)
-        })
-
-    }, [])
-
-    return (
-        <div className="max-w-[1200px] mt-20 mx-auto">
-            {submitAssignment.length}
-            <div>
-                {
-                        submitAssignment?.map(submit => <ShowSubmittedAssignment key={submit.id} submit={submit}></ShowSubmittedAssignment>)
-                }
-            </div>
-        </div>
-    );
+      <div className="table overflow-x-auto">
+        {submitAssignment?.map((submit) => (
+          <ShowSubmittedAssignment
+            key={submit.id}
+            submit={submit}
+          ></ShowSubmittedAssignment>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SubmittedAssignments;
