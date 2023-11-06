26c4dda9615b94142c91137149aec09d376a171d
zinolot <mrjahidhassan0@gmail.com>
2023-11-07 04:42:30 +0600

    filter added in all assignment section

--- src/Pages/Assignments/Assignments.jsx
+++ src/Pages/Assignments/Assignments.jsx
@@ -1,23 +1,52 @@
 import { useEffect, useState } from "react";
-import ShowAssignments from "./showAssignments";
+import ShowAssignments from "./ShowAssignments";
 
 const Assignments = () => {
   const [assignments, setAssignments] = useState([]);
+  const [filter, setFilter] = useState("all");
+
   useEffect(() => {
     fetch("http://localhost:5000/api/v1/allassignments")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setAssignments(data);
       });
   }, []);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredAssignments = assignments.filter((assignment) => {
+    if (filter === "all") {
+      return true; 
+    } else {
+      return assignment.difficultyLevel === filter;
+    }
+  });
+
   return (
     <div className="max-w-[1200px] mx-auto">
+      <div>
+        <label htmlFor="filter">Filter by Difficulty:</label>
+        <select
+          id="filter"
+          name="filter"
+          className="border ml-5 text-center px-22 my-5"
+          onChange={handleFilterChange}
+          value={filter}
+        >
+          <option value="all">All</option>
+          <option value="Easy">Easy</option>
+          <option value="Medium">Medium</option>
+          <option value="Hard">Hard</option>
+        </select>
+      </div>
       <div className="grid grid-cols-3 gap-5">
-        {assignments.map((assignments) => (
+        {filteredAssignments.map((assignment) => (
           <ShowAssignments
-            assignments={assignments}
-            key={assignments.id}
+            assignments={assignment}
+            key={assignment.id}
           ></ShowAssignments>
         ))}
       </div>
