c0707ff479daecf91aceb6bff5e45d0ce214cab6
zinolot <mrjahidhassan0@gmail.com>
2023-11-09 05:39:56 +0600

    some minor changes of footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,10 +22,10 @@ const Footer = () => {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="https://flowbite.com/" className="hover:underline">Book Hub</a>
+                          <a href="https://flowbite.com/" className="hover:underline">FlowBite</a>
                       </li>
                       <li>
-                          <a href="https://tailwindcss.com/" className="hover:underline">Wikipedia</a>
+                          <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
                       </li>
                   </ul>
               </div>
